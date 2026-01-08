770a5161573f3a3d6277c7c5e59a459ac4bfde95
metsas olev slaav jemeljanov <jemeljanov0@gmail.com>
2026-01-08 13:44:25 +0200

    IF ELSE

---
+++ IF_loogilisedoperaatorid_tingimus/IF-ELSEIF-ELSE/ELSE IF/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ELSE_IF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kui pikk sa oled? sissta sentimeetrites");
+            int pikkus = int.Parse(Console.ReadLine());
+            if (pikkus < 0)
+            {
+                Console.WriteLine("Slline pikkus on võimatu");
+            }
+            else if (pikkus < 100)
+            {
+                Console.WriteLine("Olen juntsu, hit the gym");
+            }
+            else if (pikkus < 200)
+            {
+                Console.WriteLine("Oled normi pikkusega kolge");
+            }
+            else if (pikkus > 200)
+            {
+                Console.WriteLine("Ossaraks, vaat kus hiiglane!");
+            }
+        }
+    }
+}
