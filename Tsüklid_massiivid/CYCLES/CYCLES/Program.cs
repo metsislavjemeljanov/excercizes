c66b350a27c6277e4b2afd92d90a544632991eda
Opilane TTHK <opilane@tthk.ee>
2026-01-09 13:35:55 +0200

    Lahendasin ülesande CYCLES Do-While-1 koos vooskeemiga

--- Tsüklid_massiivid/CYCLES/CYCLES/Program.cs
+++ Tsüklid_massiivid/CYCLES/CYCLES/Program.cs
@@ -4,20 +4,30 @@
     {
         static void Main(string[] args)
         {
-            //for 2. Root
-            int ruuduKülg = 0;
-            Console.WriteLine("Kui suurt ruutu sa tahad");
-            ruuduKülg = int.Parse(Console.ReadLine());
-            for (int i = 0; i < ruuduKülg; i++)
+            //for 2.
+            //int ruuduKülg = 0;
+            //Console.WriteLine("Kui suurt ruutu sa tahad");
+            //ruuduKülg = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < ruuduKülg; i++)
+            //{
+            //    string seeRida = "";
+            //    for(int j = 0; j < ruuduKülg; j++)
+            //    {
+            //        seeRida += "HH";
+            //    }
+            //    Console.WriteLine(seeRida);
+            //}
+            //Console.WriteLine("Tsüklid on lõpetanud");
+
+            //Do-While 1.
+            int täisArv = 0;
+            do
             {
-                string seeRida = "";
-                for(int j = 0; j < ruuduKülg; j++)
-                {
-                    seeRida += "HH";
-                }
-                Console.WriteLine(seeRida);
-            }
-            Console.WriteLine("Tsüklid on lõpetanud");
+                täisArv -= 2;
+                Console.WriteLine(täisArv);
+            } while (täisArv > -31);
+            Console.WriteLine("Tsükkel lõppes");
+
         }
     }
 }
