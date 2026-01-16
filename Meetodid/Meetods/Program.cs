76310799fd58a332f88077f8de5437b87d4192ad
Opilane TTHK <opilane@tthk.ee>
2026-01-16 14:08:34 +0200

    Methods

---
+++ Meetodid/Meetods/Program.cs
@@ -0,0 +1,124 @@
+﻿
+
+namespace Meetods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * kirjuta programm mis
+             * küsib kasutajalt tema lemmikfilme, küsimine ise on meetodiga, meetodi tulemus tagastatakse muutujasse
+             * küsib kasutajalt pealse seda tema lemmikfilmi, vastus hoitakse muutujas
+             * programm kontrollib meetodiga, kas lemmikfilm asub kasutaja poolt sisestatud filmide hulgas
+             * kui ei ole, siis küsitakse, kus sinu lemmikfilm?
+             * kui on siis öeldakse Näe mäletasid ikka!
+             * programm küsib kasutajalt kas talle meeldib film X (programmeerija otsustab)
+             * Kui jah siis, lisatakse film järjendisse
+             * kui ei vingub programm vastu "aga see on ju hea film?"
+             * on olemas kindlate filmide tuvastusmeetod, mis otsib, kas järjendis on "Terminator", "Vanamehe Film" või "Kratt"
+             * Erinevate filmide kohta esitab meetod erineva sõnumi
+             *Terminaatori kohta "Ill be back"
+             *Vanamehe Filmi kohta "šnikurs"
+             *Krati kohta "Vaata et ta sul tehisplära ajama ei hakka"
+             */
+
+            List<string> movies = GetMovies();
+            foreach (var movie in movies)
+            {
+                Console.WriteLine(movie);
+            }
+            Console.WriteLine("Millinne on sinu lemmikfilm?");
+            string lemmikfilm = Console.ReadLine();
+
+            DoesFavouriteMovieExist(lemmikfilm, movies);
+
+            Console.WriteLine("Aga kas sulle meeldiks äkki 67 ");
+            string lisafilm = Console.ReadLine();
+            if (lisafilm == "jah")
+            {
+                movies.Add("");
+            }
+            else
+            {
+                Console.WriteLine("Aga miks, see on ju heea film???????");
+            }
+
+            string messeges = FindFilems(movies);
+
+
+
+
+
+        }
+        
+        private static string FindFilmes(LinkedList<string> movies)
+        {
+            string foundmovies = "";
+                
+            foreach (var movie in movies)
+            {
+                if (movie == "Terminator")
+                {
+                    foundmovies += "I´ll be back\n";
+                }
+                if (movie == "Vanamehe Film")
+                {
+                    foundmovies += "Šnikurš!!\n";
+                }
+                if (movie == "Kratt")
+                {
+                    foundmovies += "Vaara et tehisplära ajama eri hakka...\n";
+                }
+            }
+            return foundmovies;
+        }
+
+        private static string FindFilems(List<string> movies)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void DoesFavouriteMovieExist(string? lemmikflm, List<string> movies)
+        {
+            bool doesItExistInList = false;
+            for (int i = 0; i < movies.Count; i++)
+            {
+                if (lemmikflm == movies[i])
+                {
+                    doesItExistInList = true;
+                    break;
+                }
+            }
+
+            if (doesItExistInList == true)
+            {
+                Console.WriteLine("Näe mäletasiud ikka!");
+            }
+            else
+            {
+                Console.WriteLine("Aga kus sinu lemmikfilm?");
+            }
+        }
+        
+
+        public static List<string> GetMovies()
+        {
+            var movies = new List<string>();
+
+            string sisestus = "";
+            while (sisestus != "ei ole")
+            {
+                Console.WriteLine("Palun sisesta järgmine film, kui rohkem ei ole, siis kirjuta \"ei ole\"");
+                sisestus = Console.ReadLine();
+                if (sisestus != "ei ole")
+                {
+                    movies.Add(sisestus);  
+                }
+                
+            }
+
+            return movies;
+        }
+    }
+}
---
+++ Meetodid/Metods 2/Program.cs
@@ -0,0 +1,77 @@
+﻿
+
+namespace Metods_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float arv1 = 0;
+            float arv2 = 0;
+            string tehtetüüp = "";
+            string uusTehe = "Jah";
+            while (uusTehe == "Jah")
+            {
+                double tulemus = 0;
+                GetUserData(arv1, arv2, tehtetüüp);
+                switch(tehtetüüp)
+                {
+                    case "+":
+                        tulemus = Liida(arv1, arv2);
+                        break;
+                    case "-":
+                        tulemus = Lahuta(arv1, arv2);
+                        break;
+                    case "*":
+                        tulemus = Korruta(arv1, arv2);
+                        break;
+                    case "/":
+                        tulemus = Jaga(arv1, arv2);
+                        break;
+                    default:
+                        Console.WriteLine("Ei tunne tehtetüüpi");
+                        break;
+                }
+            }
+        }
+
+        private static double Liida(float arv1, float arv2)
+        {
+            return arv1 + arv2;
+        }
+
+        private static double Lahuta(float arv1, float arv2)
+        {
+            return arv1 - arv2;
+        }
+
+        private static double Korruta(float arv1, float arv2)
+        {
+            return arv1 * arv2;
+        }
+
+        private static double Jaga(float arv1, float arv2)
+        {
+            return arv1 / arv2;
+        }
+
+        private static void GetUserData(float arv1, float arv2, string tehtetüüp)
+        {
+            while (arv1 == 0)
+            {
+                Console.WriteLine("Palun sisesta esimene arv");
+                arv1 = int.Parse(Console.ReadLine());
+            }
+            while (arv2 == 0)
+            {
+                Console.WriteLine("Palun sisesta teine arv");
+                arv2 = int.Parse(Console.ReadLine());
+            }
+            while (tehtetüüp == "")
+            {
+                Console.WriteLine("Palun sisesta tehtetüüp");
+                tehtetüüp = Console.ReadLine();
+            }
+        }
+    }
+}
