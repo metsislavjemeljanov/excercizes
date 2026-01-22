3037ec592b08e49b105118cae10f7f1a16bfc6fa
Opilane TTHK <rasmus.olesk12@gmail.com>
2026-01-22 13:58:13 +0200

    methods file 2

---
+++ FailOperations2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+namespace FailOperations2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayThisFile();
+            FindWord("die");
+
+        }
+
+        private static void FindWord(string filter)
+        {
+            using (StreamReader thisFile = new StreamReader("C:\\Users\\opilane\\source\\repos\\Metšislav jemeljanov\\excercizes\\FailOperations2\\laulusõnad.txt"))
+            {
+                int lnr = 0;
+                while (thisFile.EndOfStream == false)
+                {
+                    string thisLine = thisFile.ReadLine();
+                    lnr++;
+                    if (thisLine.Contains(filter))
+                    {
+                        Console.WriteLine("Sõna leitud realt #"+lnr+"");
+                    }
+                }
+                thisFile.Close();
+            }
+        }
+
+        private static void DisplayThisFile()
+        {
+            string rida = "";
+            using (StreamReader thisFile = new StreamReader("C:\\Users\\opilane\\source\\repos\\Metšislav jemeljanov\\excercizes\\FailOperations2\\laulusõnad.txt"))
+            {
+                int currentRow = 0;
+                while(thisFile.EndOfStream == false)
+                {
+                    currentRow++;
+                    rida = thisFile.ReadLine();
+                    int pikkus = rida.Length;
+                    Console.WriteLine($"#{currentRow} pikkus: {pikkus} tekst: {rida}");
+                }
+                thisFile.Close();
+            }
+        }
+    }
+}
